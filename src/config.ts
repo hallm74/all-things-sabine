2dfc685c660ee401c565e82bd04e8bb7318506f9
hallm74 <68484824+hallm74@users.noreply.github.com>
2024-12-14 22:03:00 -0600

    changes to github site

--- src/config.ts
+++ src/config.ts
@@ -34,7 +34,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/hallm74",
+    href: "https://github.com/hallm74/all-things-sabine",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
